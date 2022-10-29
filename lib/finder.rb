89cc0264026b99a8dd19d0ea2d57d6ae57a70374
Claudio Francisco das Neves <cfneguacu@uni9.edu.br>
2022-10-29 12:50:55 -0300

    Finder Ruby

--- lib/finder.rb
+++ lib/finder.rb
@@ -1,4 +1,12 @@
 class Finder
   def index_of(list, value)
+  a = Array.new
+  for i in 0...list.length do
+    if list[i].match?(value) == true
+     a[i] = i
+    end 
+  end
+  a.delete(nil)  
+  a
   end
 end
